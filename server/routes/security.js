fe2115d80c1a6670f9ec5ca8a72c6df4933896a5
Brett Grashorn <brettgrashorn@gmail.com>
2023-09-18 21:05:27 -0500

    Register API added

--- server/routes/security.js
+++ server/routes/security.js
@@ -15,6 +15,7 @@ const Ajv = require('ajv')
 
 const router = express.Router()
 const ajv = new Ajv()
+const saltRounds = 10
 
 const signinSchema = {
   type: 'object',
@@ -23,6 +24,32 @@ const signinSchema = {
     password: { type: 'string'}
   },
   required: ['email', 'password'],
+  additionalProperties: false
+}
+
+const securityQuestionsSchema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    properties: {
+      question: { type: 'string' },
+      answer: { type: 'string' }
+    },
+    required: ['question', 'answer'],
+    additionalProperties: false
+  }
+}
+
+const registerSchema = {
+  type: 'object',
+  properties: {
+    email: { type: 'string'},
+    password: { type: 'string'},
+    firstName: { type: 'string'},
+    lastName: { type: 'string'},
+    selectedSecurityQuestions: securityQuestionsSchema
+  },
+  required: ['email', 'password', 'firstName', 'lastName', 'selectedSecurityQuestions'],
   additionalProperties: false
 }
 
@@ -83,6 +110,77 @@ router.post('/signin', (req, res, next) => {
     }, next)
   } catch (err) {
     console.log('err')
+    next(err)
+  }
+})
+
+/**
+ * register
+ */
+router.post('/register', (req, res, next) => {
+  try {
+    const { user } = req.body
+    console.log('user', user)
+
+    const validate = ajv.compile(registerSchema)
+    const valid = validate(user)
+
+    if(!valid) {
+      const err = new Error('Bad Request')
+      err.status = 400
+      err.errors = validate.errors
+      console.log('user validation errors', validate.errors)
+      next(err)
+      return
+    }
+
+    user.password = bcrypt.hashSync(user.password, saltRounds)
+
+    mongo(async db => {
+
+      const users = await db.collection('users')
+      .find()
+      .sort({ empId: 1 }) //sort the records in ascending order
+      .toArray()
+
+      console.log("Employees List", users)
+
+      const userExists = users.find(emp => emp.email === users.email)
+
+      if (userExists) {
+        const err = new Error('Bad Request')
+        err.status = 400
+        err.message = 'User  already exists'
+        console.log('User already exists', err)
+        next(err)
+        return
+      }
+
+      const lastUser = users[users.length - 1]
+      const newEmpId = lastUser.empId + 1
+
+      const newUser = {
+        empId: newEmpId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password,
+        role: 'standard',
+        selectedSecurityQuestions: user.selectedSecurityQuestions
+      }
+
+      console.log('User to be inserted into MongoDB', newUser)
+
+      const result = await db.collection('users').insertOne(newUser)
+
+      console.log('MongoDB result:', result)
+
+      res.send({ id: result.insertedId })
+
+
+    }, next)
+  } catch(err) {
+    console.error('err', err)
     next(err)
   }
 })
